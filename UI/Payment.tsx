9da1a525c79e50eeef11a5671150a0b06a34a58c
HasithKovinda <it20032906@my.sliit.lk>
2024-02-05 23:41:04 +0530

    add payment success page

--- UI/Payment.tsx
+++ UI/Payment.tsx
@@ -1,5 +1,28 @@
-"use client";
-
+import Link from "next/link";
+import styles from "./Payment.module.css";
 export default function Payment() {
-  return <div>Payment</div>;
+  return (
+    <main className={styles.main}>
+      <div className={styles.heading}>
+        <h2>Congratulations 🎉 Your Purchased is successfully completed !</h2>
+      </div>
+      <div>
+        <img
+          src="/assert/card-payment.svg"
+          alt="payment"
+          className={styles.payment}
+        />
+      </div>
+      <div className={styles.explore}>
+        <p>
+          You want explore more tours and enjoy your life with your family 👪
+        </p>
+        <Link href="/tours">Explore More &rarr;</Link>
+      </div>
+      <div className={styles.explore}>
+        <p>You want view the booking information</p>
+        <Link href="/dashboard/bookings">View Details</Link>
+      </div>
+    </main>
+  );
 }
---
+++ app/payment/success/page.tsx
@@ -0,0 +1,5 @@
+import Payment from "@/UI/Payment";
+
+export default function page() {
+  return <Payment />;
+}
