797667e835e894db73bea607dfb6fb3fe6fbce8c
HasithKovinda <it20032906@my.sliit.lk>
2024-02-11 00:08:09 +0530

    hide component

--- app/(Home)/page.tsx
+++ app/(Home)/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
       <About />
       <Tours />
       <WhyUs />
-      <ReviewSection />
+      {/* <ReviewSection /> */}
       <NewLetter />
       <Gallery />
       <Sponsor />
