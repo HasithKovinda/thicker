f714151541efbe6d82c052cc8a7e70c5fc056a64
HasithKovinda <it20032906@my.sliit.lk>
2024-02-11 22:08:51 +0530

    add email subscribe

--- UI/NewLetter.tsx
+++ UI/NewLetter.tsx
@@ -1,7 +1,21 @@
+"use client";
+
+import { FormEvent, useRef } from "react";
 import styles from "./NewLetter.module.css";
 import { BiEnvelope, BiLogoTelegram } from "react-icons/bi";
+import toast from "react-hot-toast";
 
 function NewLetter() {
+  const ref = useRef<HTMLInputElement>(null);
+  function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    if (ref.current?.value) {
+      toast.success("News letter subscribe successfully");
+      ref.current.value = "";
+      return;
+    }
+    toast.error("Please add your email address");
+  }
   return (
     <section className={`section-center ${styles.section}`}>
       <h1>Subscribe Our News Letter</h1>
@@ -15,9 +29,9 @@ function NewLetter() {
             of the most innovative programs in the nation.
           </p>
           <div className={styles.form}>
-            <form className={styles.container}>
+            <form className={styles.container} onSubmit={handleSubmit}>
               <BiEnvelope className={styles.email} />
-              <input type="text" placeholder="Your Email Address" />
+              <input type="email" placeholder="Your Email Address" ref={ref} />
               <button className={`btn ${styles.submit}`}>
                 SubsCribe
                 <span>
