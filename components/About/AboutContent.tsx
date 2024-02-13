aba7894ad5a67c5f146a118a7b1531e848fbb02b
HasithKovinda <it20032906@my.sliit.lk>
2024-02-13 19:38:50 +0530

    optimized images

--- components/About/AboutContent.tsx
+++ components/About/AboutContent.tsx
@@ -1,13 +1,16 @@
+import Image from "next/image";
 import styles from "./AboutContent.module.css";
 
 export default function AboutContent() {
   return (
     <section className={`section-center ${styles.container}`}>
       <article>
-        <img
+        <Image
           src="/assert/about-image-3.svg"
           alt="about-image"
           className={styles.image}
+          height={600}
+          width={600}
         />
       </article>
       <article className={styles.heading}>
@@ -19,11 +22,21 @@ export default function AboutContent() {
         </p>
         <div className={styles["icons-container"]}>
           <div className={styles.feature}>
-            <img src="/assert/about-icon-1.svg" alt="about-icon-1" />
+            <Image
+              src="/assert/about-icon-1.svg"
+              height={60}
+              width={70}
+              alt="about-icon-1"
+            />
             <span>Expert Team for Support</span>
           </div>
           <div className={styles.feature}>
-            <img src="/assert/about-icon-2.svg" alt="about-icon-2" />
+            <Image
+              src="/assert/about-icon-2.svg"
+              height={60}
+              width={70}
+              alt="about-icon-2"
+            />
             <span>Urgent Support for Client</span>
           </div>
         </div>
@@ -32,7 +45,12 @@ export default function AboutContent() {
           the best outdoor experience of their lives."
         </blockquote>
         <div className={styles.footer}>
-          <img src="/assert/founder.webp" alt="founder" />
+          <Image
+            src="/assert/founder.webp"
+            height={80}
+            width={80}
+            alt="fonder"
+          />
           <div>
             <p>Jonathon bLIL</p>
             <span>Founder of treker</span>
--- components/About/Card.tsx
+++ components/About/Card.tsx
@@ -1,12 +1,22 @@
 import { guides } from "@/data/static";
 import styles from "./Card.module.css";
 import { BiPhoneCall } from "react-icons/bi";
+import Image from "next/image";
 
 export default function Card({ name, path, role }: guides) {
   return (
     <div className={styles["image-card"]}>
       <div className={styles.profile}>
-        <img src={path} alt={name} className={styles.image} />
+        <Image
+          src={path}
+          height={400}
+          width={400}
+          alt={name}
+          blurDataURL={path}
+          placeholder="blur"
+          loading="lazy"
+          className={styles.image}
+        />
       </div>
       <div className={styles.footer}>
         <div className={styles.info}>
